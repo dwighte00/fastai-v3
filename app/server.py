742fbdd0aee229d1ee6330b6938ff5947a11d3fa
dwighte00 <dwighte00@users.noreply.github.com>
2019-02-22 16:21:35 +0000

    Update model location and class names

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1MrHw2Pnz9pWPjG1dN-2B7F8fV8qfgSHC'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['albert', 'battersea', 'chelsea']
 path = Path(__file__).parent
 
 app = Starlette()

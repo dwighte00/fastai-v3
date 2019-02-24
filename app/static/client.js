b05a9fd6f071d7ff70b2608f84a9b04fbabf5704
dwighte00 <dwighte00@users.noreply.github.com>
2019-02-24 20:48:44 +0000

    Added styling for result, hid some elements

--- app/static/client.js
+++ app/static/client.js
@@ -24,6 +24,7 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
+            el('alert').className = 'alert alert-success';
             el('result-label').innerHTML = `Result = ${response['result']}`;
         }
         el('analyze-button').innerHTML = 'Analyze';
